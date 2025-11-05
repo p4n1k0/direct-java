f8716622216714d08810a09d92e6635ebb33aace
p4n1k0 <gabre.tarick@gmail.com>
2025-11-05 19:33:21 -0300

    repository e service c calculo de taxa e validacoes

---
+++ src/main/java/com/gt/projeto_teste/repository/TransferRepository.java
@@ -0,0 +1,6 @@
+package com.gt.projeto_teste.repository;
+
+import com.gt.projeto_teste.model.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<Transfer, Long> {}
